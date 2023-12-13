0ca6f0b9c24200f8efe2dabf1974c7edfb1b2735
Suyash Chaudhary <chuchuchan@Suyashs-MacBook-Pro.local>
2023-12-13 21:34:15 +0530

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log("Rendering Invoices Page");
+  return <p>Invoices Page</p>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log("Rendering Dashboard Page");
+  return <p>Dashboard Page</p>
+}
